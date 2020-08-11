fe00bc7b2972228ecc743798c2a119e93340b322
Chelsey Craig <chelsey.craig7321@gmail.com>
2020-08-10 20:02:10 -0400

    Created files to get react app running

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+
+module.exports = {
+    entry: "./src/client/index.js",
+    output: {
+      filename: '../public/bundle.js'
+    },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        use: [ 'react-hot-loader/webpack', 'babel-loader'],
+        exclude: [nodeModulesPath],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run postcss actions
+          options: {
+            plugins: function () { // postcss plugins, can be exported to postcss.config.js
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          }, {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }]
+        },
+      ]
+    },
+    node: {
+      __dirname: false,
+    }
+  };

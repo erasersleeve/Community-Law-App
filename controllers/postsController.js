5045a1cdfc77329ab5b970f30f2906760eafffb9
Tim Winters <61638296+erasersleeve@users.noreply.github.com>
2020-08-12 13:49:19 -0400

    mongo set up and seeds

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,33 +3,33 @@ const db = require("../models");
 // Defining methods for the booksController
 module.exports = {
   findAll: function(req, res) {
-    db.crimedb
+    db.CrimeDb
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.crimedb
+    db.CrimeDb
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
     console.log(req.body);
-    db.crimedb
+    db.CrimeDb
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.crimedb
+    db.CrimeDb
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.crimedb
+    db.CrimeDb
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))

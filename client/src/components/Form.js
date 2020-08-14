ecd860f37bb69881d8100fa8c83f00ffa9650bb4
Andrew Hall <andrewbhall96@gmail.com>
2020-08-14 15:21:15 -0400

    fixed bugs

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Form, FormGroup, Label, Input} from 'reactstrap';
+import {Form, FormGroup, Label, Input, CustomInput} from 'reactstrap';
 
 const submissionForm = (props) => {
   return (

05cf5138b5e8bca9050a5e6598ff719996a2c914
Andrew Hall <andrewbhall96@gmail.com>
2020-08-14 07:17:30 -0400

    added map state, click animation function, and lat lng coordinates

--- client/src/pages/Map/Map.js
+++ client/src/pages/Map/Map.js
@@ -3,14 +3,23 @@ import { Col, Row, Container } from "../../components/Grid";
 import MapContainer from "./MapContainer";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-
+import { Button } from 'reactstrap';
 
 
 function Map() {
+    const [latLng, setLatLng] = useState({lat: 39.9526, lng: -75.1652});
+
+    const getLatLng = (coordinates) => {
+        setLatLng(coordinates.latLng.toJSON());
+    };
+
     return (
         <Container fluid>
             <Navbar />
-            <MapContainer />
+            <Button>Submit</Button>
+            <Container>
+                <MapContainer clicked={(target, map, coordinates) => { getLatLng(coordinates) }} positions={latLng} />
+            </Container>
             <Footer />
         </Container>
     )

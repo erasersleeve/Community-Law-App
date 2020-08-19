5583a605ec68ef970ba1d49bb4a14a5a8209e676
Chelsey Craig <chelsey.craig7321@gmail.com>
2020-08-19 09:44:53 -0400

    removed home button from navbarlogin

--- client/src/pages/LoginForm/NavbarLogin.js
+++ client/src/pages/LoginForm/NavbarLogin.js
@@ -66,7 +66,7 @@ function NavBarLogin(props) {
                             <Nav className="mr-auto justify-content-end">
                                 {/* <Nav.Link href="/" className={location.pathname === "/login" ? "nav-link active" : "nav-link"}>Login</Nav.Link> */}
                                 {/* <Nav.Link href="/signup" className={location.pathname === "/signup" ? "nav-link active" : "nav-link"}>Signup</Nav.Link> */}
-                                <Nav.Link href="/home" className={location.pathname === "/home" ? "nav-link active" : "nav-link"}>Home</Nav.Link>
+                                {/* <Nav.Link href="/home" className={location.pathname === "/home" ? "nav-link active" : "nav-link"}>Home</Nav.Link> */}
                                 {/* <Nav.Link href="/map" className={location.pathname === "/map" ? "nav-link active" : "nav-link"}>Map</Nav.Link> */}
                                 {/* <Nav.Link href="/resources" className={location.pathname === "/resources" ? "nav-link active" : "nav-link"}>Resources</Nav.Link> */}
                                 <Nav.Link onClick={props.handleLogout} className={location.pathname === "/logout" ? "nav-link active" : "nav-link"}>Log Out</Nav.Link>
